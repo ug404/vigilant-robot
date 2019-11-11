b1c15339996a1bcc4b86cbc7d7d56a117899056e
ug <gddoarcher@yandex.ru>
2019-11-11 17:10:48 +0300

    v.1.2

--- kilo.py
+++ kilo.py
@@ -1,6 +1,10 @@
 def some_text_printer():
     print('text')
+    print(276658389)
+    print('этого не было...')
 
 
 if __name__ == '__main__':
     some_text_printer()
+
+    # '6c35b666db5d5f363847f1de7e3d723728405c66'
